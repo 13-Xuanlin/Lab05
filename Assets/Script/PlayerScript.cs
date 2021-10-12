8fb8b0dc8d69f8ee266295795334066a11548b42
13-Xuanlin <xuanlin56@hotmail.com>
2021-10-12 15:23:51 +0800

    Added PlayyerScript

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PlayerScript : MonoBehaviour
+{
+    private int Score;
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+        ScoreText.GetComponent<Text>().text = "Score: " + Score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+
+            Score += 10;
+            ScoreText.GetComponent<Text>().text = "Score: " + Score;
+            Destroy(other.gameObject);
+        }
+    }
+}
