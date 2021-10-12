2e8f2f1307e03dbc79906b5cb748cbf3b9331cc6
13-Xuanlin <xuanlin56@hotmail.com>
2021-10-12 16:02:23 +0800

    Updated scripts

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 
@@ -9,6 +10,9 @@ public class PlayerScript : MonoBehaviour
 {
     private int Score;
     public Text ScoreText;
+    private int ScoreWin;
+
+  
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +34,19 @@ public class PlayerScript : MonoBehaviour
             Score += 10;
             ScoreText.GetComponent<Text>().text = "Score: " + Score;
             Destroy(other.gameObject);
+            CheckWinCondition();
+        }
+        if (other.gameObject.tag == "Water")
+        {
+
+            SceneManager.LoadScene("GameLoseScene");
+        }
+    }
+    private void CheckWinCondition()
+    {
+        if (Score == 20) 
+        {
+            SceneManager.LoadScene("GameWinScene");
         }
     }
 }
---
+++ Assets/Script/TImer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TImer : MonoBehaviour
+{
+    public Text timeText;
+    public float levelTime;
+
+    private float elapsedTime;
+    private float levelElapsedTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelElapsedTime = levelTime;
+        timeText.text = "Time: " + levelElapsedTime.ToString("0.00");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        {
+            timeText.text = "Time:" + levelElapsedTime.ToString("0.00");
+            levelElapsedTime -= Time.deltaTime;
+            if (levelElapsedTime <= 0)
+            {
+
+                SceneManager.LoadScene("GameLoseScene");
+            }
+        }   }
+}
